b4f7eb0d2853a110c11e66fd8fac2edec5276ce6
cxy <cxy_studyholic@sjtu.edu.cn>
2022-06-27 19:28:22 +0800

    first commit

--- run_video_demo.sh
+++ run_video_demo.sh
@@ -3,5 +3,5 @@ python inference.py \
     https://download.openmmlab.com/mmdetection/v2.0/faster_rcnn/faster_rcnn_r50_fpn_1x_coco/faster_rcnn_r50_fpn_1x_coco_20200130-047c8118.pth \
     configs/body/2d_kpt_sview_rgb_img/topdown_heatmap/coco/hrnet_w48_coco_256x192.py \
     https://download.openmmlab.com/mmpose/top_down/hrnet/hrnet_w48_coco_256x192-b9e0b3ab_20200708.pth \
-    --video-path data/sample1.mp4 \
+    --video-path data/pexels-george-morina-6171336.mp4 \
     --out-root vis_results
